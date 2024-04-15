b0c43234a455b4ec2cac9eeccb4b67811c1218a2
tornike <tornikeeloshvili77@gmail.com>
2024-04-16 02:11:10 +0400

    add: logout custom hook

---
+++ frontend/src/hooks/useLogout.ts
@@ -0,0 +1,34 @@
+import { useState } from "react"
+import { useAuthContext } from "../context/AuthContext";
+import toast from "react-hot-toast";
+
+const useLogout = () => {
+    const [loading, setLoading] = useState(false);
+    const { setAuthUser } = useAuthContext()
+
+    const logout = async () => {
+        setLoading(true)
+        try {
+            const res = await fetch('/api/auth/logout', {
+                method: "POST",
+                headers: { "Context-Type": "application/json" },
+            })
+
+            const data = await res.json();
+
+            if (data.error) {
+                throw new Error(data.error)
+            }
+
+            localStorage.removeItem('chat-user')
+            setAuthUser(null)
+        } catch (e: any) {
+            toast.error(e.message)
+        } finally {
+            setLoading(false)
+        }
+    }
+    return { loading, logout }
+}
+
+export default useLogout
